d89c9e0ba85059a1a9cc090f247973b2db3bab7a
doshig <61166587+doshig@users.noreply.github.com>
2020-02-20 15:24:28 -0800

    added equipment data to HTML and staticJS and stylized with CSS a bit

--- MTRApp/MaintenanceRequestApp/static/static.js
+++ MTRApp/MaintenanceRequestApp/static/static.js
@@ -3,10 +3,37 @@
     angular.module('MRApp.demo', []).controller('MRAppController', ['$scope', '$http', DataFunction])
 
     function DataFunction($scope, $http) {
+
         $scope.MyData = [];
         $http.get('/mra/employee').then(function(response){
             $scope.MyData = response.data;
             // console.log(response.data)
         });
+        $scope.MyDataEquipment = [];
+        $http.get('/mra/equipment').then(function(response){
+            $scope.MyDataEquipment = response.data;
+            // console.log(response.data)
+            console.log($scope.MyDataEquipment)
+        });
+
+
+
+
+
+        
     }
 }());
+
+
+// (function() {
+//     'use strict';
+//     angular.module('MRApp.demo', []).controller('MRAppControllerEquipment', ['$scope', '$http', EquipmentFunction])
+
+//     function DataFunction($scope, $http) {
+//         $scope.MyDataEquipment = [];
+//         $http.get('/mra/equipment').then(function(response){
+//             $scope.MyDataEquipment = response.data;
+//             // console.log(response.data)
+//         });
+//     }
+// }());
